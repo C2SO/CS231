fcb9b51f9a45fed96faa7caf4d3264439f0fde1f
Thesius <nrahbany22@yahoo.com>
2019-04-13 09:55:44 -0400

    Added functionality

--- Assignment 2/assignment2.c
+++ Assignment 2/assignment2.c
@@ -6,9 +6,8 @@
 #include <stdio.h>
 #include <string.h>
 
-#define TRUE 1
-#define FALSE 0
-#define LARGEST 1000
+void print_title_author(char * title, char * proginfo);
+int words(const char sentence[ ]);
 
 void print_title_author(char * title, char * proginfo)
 {
@@ -16,19 +15,53 @@ void print_title_author(char * title, char * proginfo)
   printf("\n%s\n", title);
   printf("Nicholas Rahbany\nCPSC231\n%s\n\n", proginfo);
 }
+
 int main()
 {
   print_title_author("Assignment 2", 
                    "Reverse Strings & Count Letters");
 
-  char arr[100];
-  char upperArr[100];
+  char string[100];
+  int c = 0, count[26] = {0}, x;
+     
+  printf("Enter a string\n");
+  fgets(string, 100, stdin);
+    
+  while (string[c] != '\0') {
+  /** Considering characters from 'a' to 'z' only and ignoring others. */
+     
+    if (string[c] >= 'a' && string[c] <= 'z') {
+      x = string[c] - 'a';
+      count[x]++;
+    }
+     
+    c++;
+  }
 
-  printf("\nInput Text >>> ");
-  scanf("%s", arr);
+  printf("%d word(s) in this string\n", words(string));
 
-  strcpy(upperArr, arr);
-  for(char* c=upperArr; *c=toupper(*c); ++c);
+  for (c = 0; c < 26; c++)
+    if (count[c] != 0) {
+      printf("%c occurs %d times in the string.\n", c + 'a', count[c]);
+    }
+  return 0;
+}
 
-  printf("%s\n\n", upperArr);
+int words(const char sentence[ ])
+{
+    int counted = 0; // result
+
+    // state:
+    const char* it = sentence;
+    int inword = 0;
+
+    do switch(*it) {
+        case '\0': 
+        case ' ': case '\t': case '\n': case '\r': // TODO others?
+            if (inword) { inword = 0; counted++; }
+            break;
+        default: inword = 1;
+    } while(*it++);
+
+    return counted;
 }
