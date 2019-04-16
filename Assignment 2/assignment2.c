9e91f2b3c7cd0b88bad048cf22037f07715fd224
Thesius <nrahbany22@yahoo.com>
2019-04-15 23:45:31 -0400

    Updated Code

--- Assignment 2/assignment2.c
+++ Assignment 2/assignment2.c
@@ -4,6 +4,7 @@
 
 #include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 void print_title_author(char * title, char * proginfo);
@@ -21,48 +22,73 @@ int main()
   print_title_author("Assignment 2", 
                    "Reverse Strings & Count Letters");
 
-  char string[100];
-  char stringRev[100];
-  int c, c2 = 0, count[26] = {0}, x;
+  int c2 = 0;
   int countTotal[26] = {0};
-     
-  printf("Enter a string\n");
-  fgets(string, 100, stdin);
-    
-  while (string[c] != '\0') {
-  /** Considering characters from 'a' to 'z' only and ignoring others. */
-     
-    if (string[c] >= 'a' && string[c] <= 'z') {
-      x = string[c] - 'a';
-      count[x]++;
-      countTotal[x]++;
-    }
-     
-    c++;
+  char file_name[25];
+  char stringRev[100];
+  char string[100];
+  int count[26] = {0};
+
+  FILE * fp;
+  char * line = NULL;
+  size_t len = 0;
+  size_t read;
+ 
+  printf("Enter name of a file you wish to see >>> ");
+  gets(file_name);
+ 
+  fp = fopen(file_name, "r"); // read mode
+ 
+  if (fp == NULL)
+  {
+    perror("Error while opening the file.\n");
+    exit(EXIT_FAILURE);
   }
 
-  int reverseIndex, begin, end = 0;
-  while (string[reverseIndex] != '\0') {
-    reverseIndex++;
- 
-    end = reverseIndex - 1;
- 
-    for (begin = 0; begin < reverseIndex; begin++) {
-      stringRev[begin] = string[end];
-      end--;
-    }
- 
-    stringRev[begin] = '\0';
- 
-  }
-  printf("\nReversed String: %s", stringRev);
+  while ((read = getline(&line, &len, fp)) != EOF) {
 
-  printf("\n\n%d word(s) in this string\n", words(string));
+    strcpy(string, line);
 
-  for (c = 0; c < 26; c++) {
-    if (count[c] != 0) {
-      printf("%c occurs %d times in the string.\n", c + 'a', count[c]);
+    int c = 0;
+    while (string[c] != '\0') {
+    /** Considering characters from 'a' to 'z' only and ignoring others. */
+     
+      if (string[c] >= 'a' && string[c] <= 'z') {
+        int x = string[c] - 'a';
+        count[x]++;
+        countTotal[x]++;
+      }
+
+      else if (string[c] >= 'A' && string[c] <= 'Z') {
+        int x = string[c] - 'A';
+        count[x]++;
+        countTotal[x]++;
+      }
+     
+      c++;
     }
+
+    int reverseIndex, begin, end = 0;
+    while (string[reverseIndex] != '\0') {
+      reverseIndex++;
+      end = reverseIndex - 1;
+      for (begin = 0; begin < reverseIndex; begin++) {
+        stringRev[begin] = string[end];
+        end--;
+      }
+      stringRev[begin] = '\0';
+    }
+    printf("\nReversed String: %s", stringRev);
+    memset(stringRev, 0, sizeof stringRev);
+
+    printf("\n\n%d word(s) in this string\n", words(string));
+
+    for (int index = 0; index < 26; index++) {
+      if (count[index] != 0) {
+        printf("%c occurs %d times in the string.\n", index + 'a', count[index]);
+      }
+    }
+
   }
 
   printf("\n");
@@ -72,7 +98,7 @@ int main()
       printf("%c occurs %d times in total.\n", c2 + 'a', countTotal[c2]);
     }
   }
-
+  
   return 0;
 }
 
@@ -86,7 +112,7 @@ int words(const char sentence[ ])
 
     do switch(*it) {
         case '\0': 
-        case ' ': case '\t': case '\n': case '\r': // TODO others?
+        case ' ': case '\t': case '\n': case '\r':
             if (inword) { inword = 0; counted++; }
             break;
         default: inword = 1;
