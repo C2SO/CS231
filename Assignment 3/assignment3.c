23e3ce902791d34a40702f5ae80ef82ff9350345
Thesius <nrahbany22@yahoo.com>
2019-04-19 15:36:00 -0400

    Updated code

--- Assignment 3/assignment3.c
+++ Assignment 3/assignment3.c
@@ -19,18 +19,15 @@ int main(int argc, char *argv[])
 
       if (strstr(argv[i], "-") != NULL)
       {
-         if (strstr(argv[i], "e") != NULL ||
-             strstr(argv[i], "E") != NULL)
+         if (strstr(argv[i], "E") != NULL)
          {
             e = 1;
          }
-         if (strstr(argv[i], "b") != NULL ||
-             strstr(argv[i], "B") != NULL)
+         if (strstr(argv[i], "b") != NULL)
          {
             b = 1;
          }
-         if (strstr(argv[i], "n") != NULL ||
-             strstr(argv[i], "N") != NULL)
+         if (strstr(argv[i], "n") != NULL)
          {
             n = 1;
          }
@@ -46,14 +43,14 @@ int main(int argc, char *argv[])
       if (fp == NULL)
       {
          printf("%s: No such file or directory\n", file_name);
-         return 0;
-
-         while ((ch = fgetc(fp)) != EOF)
-         {
-            putchar(ch);
-         }
-         fclose(fp);
+         return -1;
       }
+
+      while ((ch = fgetc(fp)) != EOF)
+      {
+         putchar(ch);
+      }
+      fclose(fp);
    }
    return 0;
 }
