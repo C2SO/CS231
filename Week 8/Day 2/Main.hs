ed4d25e1904fdaca961ab5cd41390f1e8becf85f
Nicholas Rahbany <nrahbany22@yahoo.com>
2019-05-24 13:32:47 -0400

    Added Notes

---
+++ Week 8/Day 2/Main.hs
@@ -0,0 +1,27 @@
+import System.Environment
+import System.IO
+import Data.Maybe
+
+usage = "Hangman dictionary words guesses"
+
+testTooSmall args
+  | length args < 3 = Nothing
+  | otherwise      = Just args
+
+testTooBig args
+  | args == Nothing            = Nothing
+  | length (fromJust args) > 4 = Nothing
+  | otherwise                  = Just args
+
+play args = do
+  putStrLn $ show args
+
+
+main = do
+  argList <- getArgs
+  let bigEnough = testTooSmall argList
+  let smallEnough = testTooBig bigEnough
+  if smallEnough == Nothing
+     then putStrLn usage
+     else play (fromJust smallEnough)
+  
